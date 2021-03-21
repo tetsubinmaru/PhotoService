15f2d70010563e111ad071e0cc31696df2081830
tetsubinmaru <tesoro5963@gmail.com>
2021-03-21 16:43:21 +0900

    seventh commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from .models import Photo
 
 def index(request):
-    return render(request, 'app/index.html')
+    photos = Photo.objects.all().order_by('-created_at')
+    return render(request, 'app/index.html', {'photos': photos})
 
 def users_detail(request, pk):
     user = get_object_or_404(User, pk=pk)
