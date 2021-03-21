4fc78ea2c96c562032a2e2f67adf2e03898eb040
tetsubinmaru <tesoro5963@gmail.com>
2021-03-21 11:36:51 +0900

    sixth commit

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Photo
 
-# Register your models here.
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+
+admin.site.register(Photo, PhotoAdmin)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'app/index.html')
+
+def users_detail(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    return render(request, 'app/users_detail.html', {'user': user})
