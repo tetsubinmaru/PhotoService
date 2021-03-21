fafd25782849a53d4a7569b9cf6c1cce5787f86a
tetsubinmaru <tesoro5963@gmail.com>
2021-03-21 22:48:38 +0900

    master

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Photo
+from .models import Photo, Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
 
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
 
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Photo, PhotoAdmin)
--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
 
 class Photo(models.Model):
     title = models.CharField(max_length=150)
     comment = models.TextField(blank=True)
     image = models.ImageField(upload_to='photos')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
